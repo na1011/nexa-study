f235a0bc5b2992f240d4639c809c2fc68f7cd2fd
na1011 <zerus94@naver.com>
2024-05-23 17:55:23 +0900

    initail commit

---
+++ nexa-side/src/main/java/nexaStudy/controller/HomeController.java
@@ -0,0 +1,13 @@
+package nexaStudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String viewHome() {
+		return "home";
+	}
+}
